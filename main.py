0d9b3ff2b8a2c6059fb217835eef3ccd5cc8d30f
Vanstorm9 <antlowhur@yahoo.com>
2019-05-22 21:49:43 -0400

    Restructuring main.py

--- main.py
+++ main.py
@@ -1,95 +1,102 @@
 import json
 import re
 
-jsonPath = 'data/SystemViewController.json'
-with open(jsonPath) as json_data:
-    data = json.load(json_data,)
 
-#class, (.) classNames , (#) identifier 
-# There are helper classes to help work with compound selectors and selector chains
+class JSONParser:
+    def __init__(self):
+      pass
 
-def parseSelChain(key,val,targetAr):
-  # To help deal with selector chains (we iterate through chain string, split it, and append target attributes in a list of search values)
-  searchList = []
-  for targetVal in targetAr:
-    # We will have to find the chain
-    delimitSplit = re.split('([#.])',targetVal)
-    if len(delimitSplit) == 3 and val == delimitSplit[0]:
-      # We retrieve list of  
-      searchList.append(delimitSplit[1] + delimitSplit[2])
+    # There are helper classes to help work with compound selectors and selector chains
 
-  return searchList
+    def parseSelChain(self,key,val,targetAr):
+      # To help deal with selector chains (we iterate through chain string, split it, and append target attributes in a list of search values)
+      searchList = []
+      for targetVal in targetAr:
+        # We will have to find the chain
+        delimitSplit = re.split('([#.])',targetVal)
+        if len(delimitSplit) == 3 and val == delimitSplit[0]:
+          # We add target attributes to a list
+          searchList.append(delimitSplit[1] + delimitSplit[2])
 
-def matchJSON(key,val,targetAr):
-  # So that we match the correct values to tags when dealing with compound chains
-  for targetVal in targetAr:
-    if len(targetVal) == 0:
-      continue
-    
-    if targetVal[1:] == val:
-      # Check to see if values match (with assumption attribute delimiter exists)
-      if key.lower() == 'identifier' and targetVal[0] == '#':
-        return True
-      elif key.lower() == 'classnames' and targetVal[0] == '.':
-        return True
-      
-    elif targetVal == val and key.lower() == 'class':
-        # Check to see if values does match and if it is a class 
-        return True
-      
-  return False
+      return searchList
 
-
-def iterateJSON(data,prevData,keyPrev,targetAr,resList,searchList,savedData):
-  # The main recursive function
-
-  if isinstance(data,dict):
-    for key,value in data.items():
-      
-      if matchJSON(key,value,targetAr):
-        # Dealing with compound selectors
-        resList.append(data)
-
-      if matchJSON(key,value,searchList)  and savedData not in resList: 
-        # Ths means we found the target attribute (classname/identifier) associated with our parent class
-        resList.append(savedData)
-        return resList
-      else:
-        if len(searchList) == 0:
-          # Based on the class we found, we extract the target attributes from the selector chain strings
-          searchList = parseSelChain(key,value,targetAr)
-          savedData = data # We take the current section of JSON and add to resList if target attribute is present
- 
-        iterateJSON(value,data,key,targetAr,resList,searchList,savedData)
+    def matchJSON(self,key,val,targetAr):
+      # So that we match the correct values to tags when dealing with compound chains
+      for targetVal in targetAr:
+        if len(targetVal) == 0:
+          continue
+        
+        if targetVal[1:] == val:
+          # Check to see if values match (with assumption attribute delimiter exists)
+          if key.lower() == 'identifier' and targetVal[0] == '#':
+            return True
+          elif key.lower() == 'classnames' and targetVal[0] == '.':
+            return True
           
-  elif isinstance(data,list):
-    for i,subdata in enumerate(data):
-      if isinstance(subdata,str):
-        if matchJSON(keyPrev,subdata,targetAr):
-          # Dealing with compound selectors
-          resList.append(prevData)
-        if matchJSON(keyPrev,subdata,searchList) and savedData not in resList:
-          resList.append(savedData)
-          return resList
+        elif targetVal == val and key.lower() == 'class':
+            # Check to see if values does match and if it is a class 
+            return True
+          
+      return False
 
-      iterateJSON(subdata,data,keyPrev,targetAr,resList,searchList,savedData)
-      
-  
-  return resList
+
+    def iterateJSON(self,data,prevData,keyPrev,targetAr,resList,searchList,savedData):
+      # The main recursive function
+      if isinstance(data,dict):
+        for key,value in data.items():
+          
+          if self.matchJSON(key,value,targetAr):
+            # Dealing with compound selectors
+            resList.append(data)
+
+          if self.matchJSON(key,value,searchList)  and savedData not in resList: 
+            # Ths means we found the target attribute (classname/identifier) associated with our parent class
+            resList.append(savedData)
+            return resList
+          else:
+            if len(searchList) == 0:
+              # Based on the class we found, we extract the target attributes from the selector chain strings
+              searchList = self.parseSelChain(key,value,targetAr)
+              savedData = data # We take the current section of JSON and add to resList if target attribute is present
+     
+            self.iterateJSON(value,data,key,targetAr,resList,searchList,savedData)
+              
+      elif isinstance(data,list):
+        for i,subdata in enumerate(data):
+          if isinstance(subdata,str):
+            if self.matchJSON(keyPrev,subdata,targetAr):
+              # Dealing with compound selectors
+              resList.append(prevData)
+            if self.matchJSON(keyPrev,subdata,searchList) and savedData not in resList:
+              resList.append(savedData)
+              return resList
+
+          self.iterateJSON(subdata,data,keyPrev,targetAr,resList,searchList,savedData)
+      return resList
   
 def main():
+
+  jsonPath = 'data/SystemViewController.json'
+  with open(jsonPath) as json_data:
+    data = json.load(json_data,)
+    
   # subviews, contentView, input and control
   print("Type in a JSON selector input string:")
   inputStr = input()
 
   inputAr = inputStr.split(" ") # For compund selector
 
-  result = iterateJSON(data,data,'',inputAr,[], [],None)
+  parser = JSONParser()
+
+  result = parser.iterateJSON(data,data,'',inputAr,[], [],None)
 
   for i,resEle in enumerate(result):
     print("------------------")
     print(i, ": ", resEle)
   return
+
+
+
 
 if __name__ == "__main__":
   main()
